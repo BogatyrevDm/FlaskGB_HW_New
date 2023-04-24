1b9458ceea19fb3a782ed26cfc26c4abfc2b4f40
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-04-24 22:15:10 +0300

    register form

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, login_user
 from werkzeug.exceptions import NotFound
+from werkzeug.security import generate_password_hash
 
+from blog.extensions import db
 from blog.forms.user import UserRegisterForm
 from blog.models import User
 
@@ -14,10 +16,26 @@ def register():
         return redirect(url_for('user.profile', pk=current_user.id))
 
     form = UserRegisterForm(request.form)
+    errors = []
+    if request.method == 'POST' and form.validate_on_submit():
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append('email not uniq')
+            return render_template('users/register.html', form=form)
+
+        _user = User(email=form.email.data,
+                     first_name=form.first_name.data,
+                     last_name=form.last_name.data,
+                     password=generate_password_hash(form.password.data)
+                     )
+
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
 
     return render_template(
         'users/register.html',
-        form=form
+        form=form, errors=errors
     )
 
 
