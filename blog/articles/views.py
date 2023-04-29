32dd4b8c43e058d53fd85568c5ea7763da2ffc6c
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-04-29 14:22:08 +0300

    adding tags

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, url_for, redirect
 from flask_login import login_required, current_user
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
 from blog.extensions import db
 from blog.forms.article import CreateArticleForm
-from blog.models import Article, Author
+from blog.models import Article, Author, Tag
 
 article = Blueprint('article', __name__, url_prefix='/articles', static_folder='../static')
 
@@ -20,7 +21,12 @@ def article_list():
 
 @article.route('/<int:article_id>', methods=['GET'])
 def article_detail(article_id):
-    _article: Article = Article.query.filter_by(id=article_id).one_or_none()
+    _article: Article = Article.query.filter_by(
+        id=article_id
+    ).options(
+        joinedload(Article.tags)
+    ).one_or_none()
+
     if not _article:
         raise NotFound
     return render_template(
@@ -33,6 +39,7 @@ def article_detail(article_id):
 @login_required
 def create_article_form():
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
     return render_template('articles/create.html', form=form)
 
 
@@ -40,17 +47,24 @@ def create_article_form():
 @login_required
 def create_article():
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
 
     if form.validate_on_submit():
         _article = Article(title=form.title.data.strip(), text=form.text.data)
-        db.session.add(_article)
         if current_user.author:
-            _article.author = current_user.author
+            _article.author_id = current_user.author.id
         else:
             author = Author(user_id=current_user.id)
             db.session.add(author)
-            _article.author = current_user.author
+            db.session.flush()
+            _article.author_id = author.id
 
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                _article.tags.append(tag)
+
+        db.session.add(_article)
         db.session.commit()
 
         return redirect(url_for('article.details', article_id=_article.id))
--- blog/commands.py
+++ blog/commands.py
@@ -14,3 +14,18 @@ def create_init_user():
             User(email='name@example.com', password=generate_password_hash('test123'))
         )
         db.session.commit()
+
+
+@click.command('create-init-tags')
+def create_init_tags():
+    from blog.models import Tag
+    from wsgi import app
+
+    tags = ('flask', 'django', 'python', 'geekbrains', 'sqllite')
+
+    with app.app_context():
+        for item in tags:
+            tag = Tag(name=item)
+            db.session.add(tag)
+        db.session.commit()
+    click.echo(f'Created tags {", ".join(tags)}')
